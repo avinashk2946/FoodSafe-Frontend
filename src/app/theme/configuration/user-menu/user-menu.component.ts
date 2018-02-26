f06983c4ba8affa515cc7179959790164f6b1126
sambit panda <sambit.panda@airtel.com>
2018-02-26 22:27:49 +0530

    user menu format define

--- src/app/theme/configuration/user-menu/user-menu.component.ts
+++ src/app/theme/configuration/user-menu/user-menu.component.ts
@@ -22,12 +22,12 @@ export class ProductTreeviewConfig extends TreeviewConfig {
 
 
 @Component({
-  selector: 'app-configuration',
-  templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss'],
-  providers:[UserMenuService,
-  { provide: TreeviewEventParser, useClass: OrderDownlineTreeviewEventParser },
-        { provide: TreeviewConfig, useClass: ProductTreeviewConfig }]
+    selector: 'app-configuration',
+    templateUrl: './user-menu.component.html',
+    styleUrls: ['./user-menu.component.scss'],
+    providers:[UserMenuService,
+    { provide: TreeviewEventParser, useClass: OrderDownlineTreeviewEventParser },
+    { provide: TreeviewConfig, useClass: ProductTreeviewConfig }]
 })
 
 
@@ -41,92 +41,133 @@ export class UserMenuComponent implements OnInit {
 // public dyanmicmenu:DyanmicMenu;
 
 
-    constructor(
-        private service: UserMenuService
+constructor(
+    private service: UserMenuService
     ) { }
 
-    ngOnInit() {
-        this.items = this.service.getProducts();
-    }
-    
-    onSelectedChange(downlineItems: DownlineTreeviewItem[]) {
-        this.rows = [];
-        downlineItems.forEach(downlineItem => {
-            const item = downlineItem.item;
-            const value = item.value;
-            const texts = [item.text];
-            let parent = downlineItem.parent;
-            console.log('item',item);
-            console.log('value',value);
-            console.log('texts',texts);
-            console.log('parent',parent);
-            while (!_.isNil(parent)) {
-                texts.push(parent.item.text);
-                parent = parent.parent;
-            }
-            const reverseTexts = _.reverse(texts);
-            const row = `${reverseTexts.join(' -> ')} : ${value}`;
-            this.rows.push(row);
-        });
-    }
-    removeItem(item: TreeviewItem) {
-        let isRemoved = false;
-        for (const tmpItem of this.items) {
-            if (tmpItem === item) {
-                _.remove(this.items, item);
-            } else {
-                isRemoved = TreeviewHelper.removeItem(tmpItem, item);
-                if (isRemoved) {
-                    break;
-                }
-            }
-        }
+ngOnInit() {
+    this.items = this.service.getProducts();
+     const MENUITEMS = [ 
+{
+   name:'Regulatory',
+   state:'Regulatory',
+   isEdit:true,
+   isShow:true
+},
+{
+   name:'Prerequisite Program',
+   state:'PrerequisiteProgram',
+   isEdit:true,
+   isShow:true
+},
+{
+   name:'Operating Procedures',
+   state:'OperatingProcedures',
+   isEdit:true,
+   isShow:true
+},
+{
+   name:'Record Keeping',
+   state:'RecordKeeping',
+   isEdit:true,
+   isShow:true,
+   children:[
+      {
+         name:'Raw Materials',
+         state:'rawmaterial',
+         isEdit:true,
+         isShow:true
+      },
+      {
+         name:'Packaging',
+         state:'Packaging',
+         isEdit:true,
+         isShow:true
+      },
+      {
+         name:'Chemicals',
+         state:'Chemicals',
+         isEdit:true,
+         isShow:true
+      },
+      {
+         name:'Returns',
+         state:'Returns',
+         isEdit:true,
+         isShow:true
+      }
+   ]
+},
+{
+   name:'Configuration',
+   state:'configuration',
+    isEdit:true,
+    isShow:true,
+   children:[
+      {
+         name:'login page',
+         state:'login-page',
+        isEdit:true,
+        isShow:true
+      },
+      {
+         name:'user menu',
+         state:'user-menu',
+         isEdit:true,
+        isShow:true
+      }
+   ]
 
-        if (isRemoved) {
-            this.treeviewComponent.raiseSelectedChange();
+}
+];
+    const USERTYPE = [
+    { id: 'OPERATOR', state: 'Operator'},
+    { id: 'Admin', state: 'Admin'}
+    ];
+}
+
+onSelectedChange(downlineItems: DownlineTreeviewItem[]) {
+    this.rows = [];
+    downlineItems.forEach(downlineItem => {
+        const item = downlineItem.item;
+        const value = item.value;
+        const texts = [item.text];
+        let parent = downlineItem.parent;
+        console.log('item',item);
+        console.log('value',value);
+        console.log('texts',texts);
+        console.log('parent',parent);
+        while (!_.isNil(parent)) {
+            texts.push(parent.item.text);
+            parent = parent.parent;
+        }
+        const reverseTexts = _.reverse(texts);
+        const row = `${reverseTexts.join(' -> ')} : ${value}`;
+        this.rows.push(row);
+    });
+}
+removeItem(item: TreeviewItem) {
+    let isRemoved = false;
+    for (const tmpItem of this.items) {
+        if (tmpItem === item) {
+            _.remove(this.items, item);
+        } else {
+            isRemoved = TreeviewHelper.removeItem(tmpItem, item);
+            if (isRemoved) {
+                break;
+            }
         }
     }
+
+    if (isRemoved) {
+        this.treeviewComponent.raiseSelectedChange();
+    }
+}
     //values = $event
    /* ngOnInit() {
         this.items = this.service.getBooks();
     }*/
 
 
-// const MENUITEMS = [
-//   {
-//     label: '',
-//     main: [
-//       {
-//         state: '',
-//         short_label: 'H',
-//         name: 'Hazard Analysis',
-//         type: 'sub',
-//         icon: 'icon-direction-alt',
-//         children: [
-//           {
-//             state: 'riskassessment',
-//             name: 'Risk Assessment',
-//             type: 'sub',
-//           }, {
-//             state: 'monitoringdetails',
-//             name: 'Monitoring Details',
-//             type: 'sub',
-//             // children: [
-//             //   {
-//             //     state: '',
-//             //     name: '',
-//             //   },
-//             //   {
-//             //     state: '',
-//             //     name: '',
-//             //   }
-//             // ]
-           
-//           },
 
-//           ]
-//         },
-//       ]
-//     }
-//   ];
 }
