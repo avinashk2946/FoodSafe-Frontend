37c7777b9ae68bf0b715bd2b35b64c430de4d2c8
Abinash <abinashbehrabls@gmail.com>
2018-03-29 22:58:08 +0530

    small-changes

--- src/app/theme/Record-keeping/raw-materials/document-upload/sample-collection/sample-collection.component.ts
+++ src/app/theme/Record-keeping/raw-materials/document-upload/sample-collection/sample-collection.component.ts
@@ -24,75 +24,66 @@ export class SampleCollectionComponent implements OnInit {
   }
   
   samples = [
-    {
-      sampplingno: "1",
-      supplierlot: "32123",
-      pictureofcaselabel: "",
-      qcanalysissampling: false,
-      microsampling:  false,
-      virussampling: false,
-      pesticidesampling: false,
-      indicatororganismsampling: false
-    },
+    // {
+    //   sampplingno: "1",
+    //   supplierlot: "32123",
+    //   pictureofcaselabel: "",
+    //   qcanalysissampling: false,
+    //   microsampling:  false,
+    //   virussampling: false,
+    //   pesticidesampling: false,
+    //   indicatororganismsampling: false
+    // },
+  {
+       "supplierLot"         :"testLot1",
+       "newLot"              :"true",
+       "po"                  :"po1",
+       "totalQuantity"       : 10,
+       "quantityPlanned"     :10,
+       "qualityAnalysis"     :false,
+
+     
+       "indicatorTest"       :false,
+       "pathogenTest"        :false,
+       "virusTest"           :false,
+       "pesticideTest"       :false,
+        "comment" :"samples are uploaded properly"
+       
+     },
+        {
+       "supplierLot"         :"testLot2",
+       "newLot"              :"false",
+       "po"                  :"po234",
+       "totalQuantity"       : 9,
+       "quantityPlanned"     :10,
+       "qualityAnalysis"     :true,
+
+     
+       "indicatorTest"       :true,
+       "pathogenTest"        :true,
+       "virusTest"           :true,
+       "pesticideTest"       :true,
+       "comment":"samples are uploaded properly"
+
+       
+
+     },
+     {
+       "supplierLot"         :"testLot2",
+       "newLot"              :"false",
+       "po"                  :"Po123",
+       "totalQuantity"       : 10,
+       "quantityPlanned"     :10,
+       "qualityAnalysis"     :false,
+
+     
+       "indicatorTest"       :false,
+       "pathogenTest"        :false,
+       "virusTest"           :false,
+       "pesticideTest"       :false,
+       "comment":"some samples are uploaded properly"
+     }
   ];
 
-
-
-//    samples : [
-//      {
-//        "supplierLot"         :"testLot1",
-//        "newLot"              :"true",
-//        "po"                  :"po1",
-//        "totalQuantity"       : 10,
-//        "quantityPlanned"     :10,
-//        "qualityAnalysis"     :false,
-
-     
-//        "indicatorTest"       :false,
-//        "pathogenTest"        :false,
-//        "virusTest"           :false,
-//        "pesticideTest"       :false,
-//         "comment" :"samples are uploaded properly"
-       
-
-//      },
-//      {
-//        "supplierLot"         :"testLot2",
-//        "newLot"              :"false",
-//        "po"                  :"po234",
-//        "totalQuantity"       : 9,
-//        "quantityPlanned"     :10,
-//        "qualityAnalysis"     :true,
-
-     
-//        "indicatorTest"       :true,
-//        "pathogenTest"        :true,
-//        "virusTest"           :true,
-//        "pesticideTest"       :true,
-//        "comment":"samples are uploaded properly"
-
-       
-
-//      },
-//      {
-//        "supplierLot"         :"testLot2",
-//        "newLot"              :"false",
-//        "po"                  :"Po123",
-//        "totalQuantity"       : 10,
-//        "quantityPlanned"     :10,
-//        "qualityAnalysis"     :false,
-
-     
-//        "indicatorTest"       :false,
-//        "pathogenTest"        :false,
-//        "virusTest"           :false,
-//        "pesticideTest"       :false,
-//        "comment":"some samples are uploaded properly"
-
-       
-
-//      }
-//    ]
-// }
 
 }
