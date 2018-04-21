7ac3233bfaa9baa8089be2f848dff3874e3390d0
Abinash <abinashbehrabls@gmail.com>
2018-04-21 16:39:02 +0530

    final-chnages

--- src/app/theme/Record-keeping/raw-materials/document-upload/document-upload.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentUploadDirective } from './document-upload.directive';
-
-describe('DocumentUploadDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DocumentUploadDirective();
-    expect(directive).toBeTruthy();
-  });
-});
